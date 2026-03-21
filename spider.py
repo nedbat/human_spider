f386a447a08da6a0c6b69d80c58dbdaf4d66092a
Ned Batchelder <ned@nedbatchelder.com>
2026-03-21 09:45:18 -0400

    Req and Resp classes

--- spider.py
+++ spider.py
@@ -11,6 +11,9 @@ import mimetypes
 import re
 import sys
 import urllib.parse
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Collection
 
 import requests
 from bs4 import BeautifulSoup
@@ -18,63 +21,115 @@ from bs4 import BeautifulSoup
 
 START = "https://sethmlarson.dev"
 
-domains_done = set()
 people = set()
 human_jsons = {}
 
 
-def get_human_json(domain: str) -> dict | None:
-    resp = requests.get(domain, timeout=10)
-    soup = BeautifulSoup(resp.content, "html.parser")
-    for item in soup.find_all("meta", {"name": "author", "content": True}):
-        if author := item["content"]:
-            print(f"{domain} author: {author!r}")
-            people.add(author)
+@dataclass
+class Resp:
+    resp: Any
 
-    for item in soup.find_all("script", {"type": "application/ld+json"}):
-        jsonld_str = item.string
-        jsonld = json.loads(jsonld_str)
-        at_type = jsonld.get("@type")
-        if at_type:
-            msg = f"{domain} has {len(jsonld_str)} chars of {at_type!r} JSON-LD"
-            if at_type == "Person":
-                name = jsonld.get("name", "")
-                if name:
-                    msg += f", name is {name!r}"
-                    people.add(name)
-            author = jsonld.get("author", {}).get("name", "")
-            if author:
-                msg += f", with author: {author!r}"
+    def soup(self):
+        return BeautifulSoup(self.resp.content, "html.parser")
+
+    @property
+    def url(self) -> str:
+        return self.resp.url
+
+    @property
+    def content(self) -> bytes:
+        return self.resp.content
+
+    def json(self) -> Any:
+        return self.resp.json()
+
+    def save(self, *, dirname: str = "") -> None:
+        filename = re.sub(r"[^\w]", "_", self.resp.url.partition("://")[-1])
+        content_type = (
+            self.resp.headers.get("content-type", "").partition(";")[0].strip()
+        )
+        ext = mimetypes.guess_extension(content_type) or ".dat"
+        with Path(dirname, f"{filename}{ext}").open("wb") as f:
+            f.write(self.resp.content)
+
+
+@dataclass
+class Req:
+    url: str
+    base: str = ""
+    fail_ok: bool = False
+    ok_errors: Collection[int] = ()
+    reason: str = ""
+
+    def get(self) -> Resp | None:
+        if self.base:
+            url = urllib.parse.urljoin(self.base, self.url)
+        else:
+            url = self.url
+        resp = requests.get(url, timeout=10, allow_redirects=True)
+        if resp.status_code != 200 and self.fail_ok:
+            return None
+        if resp.status_code in self.ok_errors:
+            return None
+        resp.raise_for_status()
+        return Resp(resp)
+
+
+def get_human_json(url: str) -> dict | None:
+    # fail_ok=True because bots might be forbidden
+    page_resp = Req(url, reason="main page", fail_ok=True).get()
+    if page_resp is not None:
+        soup = page_resp.soup()
+        for item in soup.find_all("meta", {"name": "author", "content": True}):
+            if author := item["content"]:
+                print(f"{url} author: {author!r}")
                 people.add(author)
-            print(msg)
+
+        for item in soup.find_all("script", {"type": "application/ld+json"}):
+            jsonld_str = item.string
+            jsonld = json.loads(jsonld_str)
+            at_type = jsonld.get("@type")
+            if at_type:
+                msg = f"{url} has {len(jsonld_str)} chars of {at_type!r} JSON-LD"
+                if at_type == "Person":
+                    name = jsonld.get("name", "")
+                    if name:
+                        msg += f", name is {name!r}"
+                        people.add(name)
+                author = jsonld.get("author", {}).get("name", "")
+                if author:
+                    msg += f", with author: {author!r}"
+                    people.add(author)
+                print(msg)
+        url = page_resp.url
+    else:
+        soup = None
 
     guessed = False
-    for item in soup.find_all("link", {"rel": "human-json", "href": True}):
-        hjurl = item["href"]
-        print(f"{domain} points to {hjurl}")
-        break
-    else:
+    hjurl = ""
+    if soup is not None:
+        for item in soup.find_all("link", {"rel": "human-json", "href": True}):
+            hjurl = item["href"]
+            print(f"{url} points to {hjurl}")
+            break
+    if not hjurl:
         hjurl = "/human.json"
         guessed = True
 
-    hjurl = urllib.parse.urljoin(resp.url, hjurl)
-    resp = requests.get(hjurl, timeout=10, allow_redirects=True)
-    if resp.status_code in {403, 404, 406, 410} and guessed:
+    req = Req(hjurl, base=url, reason="human.json")
+    if guessed:
+        req.ok_errors = {403, 404, 406, 410}
+    if (resp := req.get()) is None:
         return None
-    resp.raise_for_status()
 
-    filename = re.sub(r"[^\w]", "_", resp.url.partition("://")[-1])
-    content_type = resp.headers.get("content-type", "application/json").partition(";")[0].strip()
-    ext = mimetypes.guess_extension(content_type) or ".dat"
-    with open(f"data/{filename}{ext}", "wb") as f:
-        f.write(resp.content)
+    resp.save(dirname="data")
     if b"<html" in resp.content:
         if not guessed:
             error(f"{hjurl} served HTML")
         return None
 
     hj = resp.json()
-    human_jsons[hjurl] = len(hj.get("vouches", []))
+    human_jsons[resp.url] = len(hj.get("vouches", []))
     return hj
 
 
@@ -85,6 +140,7 @@ def error(msg):
 
 def main():
     domains_to_do = {START}
+    domains_done = set()
     while domains_to_do:
         domain = domains_to_do.pop()
         domains_done.add(domain)
