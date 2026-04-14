0d33ada43a0ce3004008b34d90281061efac6194
Ned Batchelder <ned@nedbatchelder.com>
2026-04-14 10:45:46 -0400

    include time-since-start in the logs

--- logs.py
+++ logs.py
@@ -1,10 +1,23 @@
 import logging
+import time
+
+
+class RelativeFormatter(logging.Formatter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.start = time.time()
+
+    def format(self, record):
+        record.relsecs = record.created - self.start
+        return super().format(record)
 
 
 fetch_log = logging.getLogger("fetch")
 fetch_log.setLevel(logging.DEBUG)
 fetch_handler = logging.FileHandler("fetch.log", mode="w")
 fetch_handler.setFormatter(
-    logging.Formatter("%(asctime)s.%(msecs)03d %(message)s", datefmt="%H:%M:%S")
+    RelativeFormatter(
+        "%(asctime)s %(relsecs)8.3f %(message)s", datefmt="%H:%M:%S"
+    )
 )
 fetch_log.addHandler(fetch_handler)
